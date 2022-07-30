7a313b068a1496fad9ae6673dee00ed6652066c1
tahaluh <thauanlucascpl@gmail.com>
2022-07-30 14:09:04 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
+import Card from "./components/cards/card";
+
+function App() {
+  const [values, setValues] = useState();
+  const [listCard, setListCard] = useState();
+
+  const handleChangeValues = (value) => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  };
+
+  const handleClickButton = () => {
+    Axios.post("http://localhost:3001/register", {
+      name: values.name,
+      description: values.description,
+      date: values.date,
+    }).then(() => {
+      Axios.post("http://localhost:3001/search", {
+        name: values.name,
+        description: values.description,
+        date: values.date,
+      }).then((response) => {
+        console.log(response.data)
+        setListCard([
+          ...listCard,
+          {
+            idtodo: response.data[(response.data.length)-1].idtodo,
+            name: values.name,
+            description: values.description,
+            date: values.date,
+            status: false,
+          },
+        ]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getTodoCards").then((response) => {
+      setListCard(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="register--container">
+        <h1 className="register--tittle">To-do List</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome da tarefa"
+          className="register--input"
+          onChange={handleChangeValues}
+        ></input>
+        <textarea
+          form="to-do"
+          name="description"
+          placeholder="Descrição"
+          className="register--input"
+          onChange={handleChangeValues}
+        ></textarea>
+        <input
+          type="date"
+          name="date"
+          placeholder="Insira seu nome"
+          className="register--input"
+          onChange={handleChangeValues}
+        ></input>
+        <button className="register-button" onClick={() => handleClickButton()}>
+          Enviar
+        </button>
+      </div>
+      {typeof listCard !== "undefined" &&
+        listCard.map((value) => {
+          return (
+            <Card
+              key={value.idtodo}
+              listCard={listCard}
+              setListCard={setListCard}
+              id={value.idtodo}
+              name={value.name}
+              description={value.description}
+              date={value.date}
+              status={value.status}
+            ></Card>
+          );
+        })}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/cards/card.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import "./card.css";
+import Axios from "axios";
+
+export default function Card(props) {
+  const [editValues, setEditValues] = useState({
+    name: props.name,
+    description: props.description,
+    date: props.date.slice(0, 10),
+    id: props.id,
+    status: props.status,
+  });
+  const [editing, setEditing] = useState(false);
+
+  const handleChangeValues = (value) => {
+    setEditValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  };
+
+  const handleClickEdit = () => {
+    setEditing((prevValue) => {
+      return !prevValue;
+    });
+  };
+
+  const handleClickCheck = (value) => {
+    setEditValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name]: value.target.checked,
+    }));
+  };
+
+  const handleClickSave = (value) => {
+    handleClickEdit();
+    Axios.put("http://localhost:3001/edit", {
+      id: editValues.id,
+      name: editValues.name,
+      description: editValues.description,
+      date: editValues.date,
+      status: editValues.status,
+    }).then(() => {
+      props.setListCard(
+        props.listCard.map((value) => {
+          return value.idtodo == editValues.id
+            ? {
+                idtodo: editValues.id,
+                name: editValues.name,
+                description: editValues.description,
+                date: editValues.date,
+                status: editValues.status,
+              }
+            : value;
+        })
+      );
+    });
+  };
+
+  const handleDeleteTodo = () => {
+    Axios.delete(`http://localhost:3001/delete/${editValues.id}`).then(() => {
+      props.setListCard(
+        props.listCard.filter((value) => {
+          return value.idtodo != editValues.id;
+        })
+      );
+    });
+  };
+
+  return (
+    <div className="card--container">
+      {!editing ? (
+        <div>
+          <label className="card--div">
+            <h1 className="card--name">{props.name}</h1>
+            <p className="card--description">{props.description}</p>
+            <p className="card--date">
+              {props.date.slice(0, 10).split("-").reverse().join("-")}
+            </p>
+          </label>
+
+          <button className="card--button" onClick={handleClickEdit}>
+            Editar
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div className="card--div">
+            <input
+              type="checkbox"
+              className="card--check"
+              name="status"
+              defaultChecked={props.status}
+              onChange={handleClickCheck}
+            ></input>
+            <input
+              type="text"
+              className="card--name"
+              name="name"
+              defaultValue={props.name}
+              onChange={handleChangeValues}
+            />
+            <textarea
+              className="card--description"
+              name="description"
+              defaultValue={props.description}
+              onChange={handleChangeValues}
+            ></textarea>
+            <input
+              type="date"
+              className="card--date"
+              name="date"
+              defaultValue={props.date.slice(0, 10)}
+              onChange={handleChangeValues}
+            />
+          </div>
+          <button className="card--button" onClick={handleClickSave}>
+            Salvar
+          </button>
+        </div>
+      )}
+      <button className="card--button" onClick={handleDeleteTodo}>
+        Apagar
+      </button>
+    </div>
+  );
+}
